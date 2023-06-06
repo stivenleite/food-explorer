e020baf8705519148df96d89dc6fcf7d968ad597
stivenleite <stivenleite@gmail.com>
2023-06-05 22:28:34 -0300

    home

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,10 +1,16 @@
-import { Container } from "./styles";
+import { Container, Banner } from "./styles";
 import { Header } from '../../components/Header';
+
+import Macarrons from '../../assets/Macarrons.png'
 
 export function Home () {
     return (
         <Container>
             <Header />
+
+            <Banner>
+                
+            </Banner>
         </Container>
     )
 }
